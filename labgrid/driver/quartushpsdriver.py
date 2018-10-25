BastianStender/labgrid

commit b422482af9dca5d31523b216c53e0d367377999e

multiple: use ManagedFile for file uploads
Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/driver/quartushpsdriver.py
@@ -10,6 +10,7 @@ from ..resource.udev import AlteraUSBBlaster
 from ..step import step
 from .common import Driver, check_file
 from .exception import ExecutionError
+from ..util.managedfile import ManagedFile
 
 
 @target_factory.reg_driver
@@ -63,8 +64,8 @@ class QuartusHPSDriver(Driver):
     def flash(self, filename=None, address=0x0):
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
-        filename = os.path.abspath(os.path.expanduser(filename))
-        check_file(filename, command_prefix=self.interface.command_prefix)
+        mf = ManagedFile(filename)
+        mf.sync_to_resource()
 
         assert isinstance(address, int)
 
@@ -73,6 +74,6 @@ class QuartusHPSDriver(Driver):
         cmd += [
             "--cable={}".format(cable_number),
             "--addr=0x{:X}".format(address),
-            "--operation=P {}".format(filename),
+            "--operation=P {}".format(mf.get_remote_path()),
         ]
         subprocess.check_call(cmd)

labgrid/driver/fastbootdriver.py
@@ -8,6 +8,7 @@ from ..resource.remote import NetworkAndroidFastboot
 from ..resource.udev import AndroidFastboot
 from ..step import step
 from .common import Driver, check_file
+from ..util.managedfile import ManagedFile
 
 
 @target_factory.reg_driver
@@ -48,16 +49,16 @@ class AndroidFastbootDriver(Driver):
     @Driver.check_active
     @step(args=['filename'])
     def boot(self, filename):
-        filename = os.path.abspath(filename)
-        check_file(filename, command_prefix=self.fastboot.command_prefix)
-        self("boot", filename)
+        mf = ManagedFile(filename, self.fastboot)
+        mf.sync_to_resource()
+        self("boot", mf.get_remote_path())
 
     @Driver.check_active
     @step(args=['partition', 'filename'])
     def flash(self, partition, filename):
-        filename = os.path.abspath(filename)
-        check_file(filename, command_prefix=self.fastboot.command_prefix)
-        self("flash", partition, filename)
+        mf = ManagedFile(filename, self.fastboot)
+        mf.sync_to_resource()
+        self("flash", partition, mf.get_remote_path())
 
     @Driver.check_active
     @step(args=['cmd'])

labgrid/driver/networkusbstoragedriver.py
@@ -8,6 +8,7 @@ from ..factory import target_factory
 from ..resource.udev import USBMassStorage, USBSDMuxDevice
 from ..resource.remote import NetworkUSBMassStorage, NetworkUSBSDMuxDevice
 from ..step import step
+from ..util.managedfile import ManagedFile
 from .common import Driver, check_file
 
 @target_factory.reg_driver
@@ -34,12 +35,12 @@ class NetworkUSBStorageDriver(Driver):
 
     @step(args=['filename'])
     def write_image(self, filename):
-        filename = os.path.abspath(filename)
-        check_file(filename, command_prefix=self.storage.command_prefix)
+        mf = ManagedFile(filename, self.storage)
+        mf.sync_to_resource()
         self.logger.info("pwd: %s", os.getcwd())
         args = [
             "dd",
-            "if={}".format(filename),
+            "if={}".format(mf.get_remote_path()),
             "of={} status=progress bs=4M conv=fdatasync"
             .format(self.storage.path)
         ]

labgrid/driver/openocddriver.py
@@ -10,6 +10,7 @@ from ..protocol import BootstrapProtocol
 from ..resource.remote import NetworkAlteraUSBBlaster
 from ..resource.udev import AlteraUSBBlaster
 from ..step import step
+from ..util.managedfile import ManagedFile
 from .common import Driver, check_file
 
 
@@ -60,9 +61,9 @@ class OpenOCDDriver(Driver, BootstrapProtocol):
     def load(self, filename=None):
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
-        filename = os.path.abspath(os.path.expanduser(filename))
+        mf = ManagedFile(filename, self.interface)
+        mf.sync_to_resource()
 
-        check_file(filename, command_prefix=self.interface.command_prefix)
         for config in self.config:
             check_file(config, command_prefix=self.interface.command_prefix)
 
@@ -71,7 +72,7 @@ class OpenOCDDriver(Driver, BootstrapProtocol):
         cmd += chain.from_iterable(("--file", path) for path in self.config)
         cmd += [
             "--command", "'init'",
-            "--command", "'bootstrap {}'".format(filename),
+            "--command", "'bootstrap {}'".format(mf.get_remote_path()),
             "--command", "'shutdown'",
         ]
         subprocess.check_call(cmd)

labgrid/driver/usbloader.py
@@ -9,6 +9,7 @@ from ..resource.remote import NetworkMXSUSBLoader, NetworkIMXUSBLoader
 from ..resource.udev import MXSUSBLoader, IMXUSBLoader
 from ..step import step
 from .common import Driver, check_file
+from ..util.managedfile import ManagedFile
 
 
 @target_factory.reg_driver
@@ -39,10 +40,11 @@ class MXSUSBDriver(Driver, BootstrapProtocol):
     def load(self, filename=None):
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
-        filename = os.path.abspath(filename)
-        check_file(filename, command_prefix=self.loader.command_prefix)
+        mf = ManagedFile(filename, self.loader)
+        mf.sync_to_resource()
+
         subprocess.check_call(
-            self.loader.command_prefix+[self.tool, "0", filename]
+            self.loader.command_prefix+[self.tool, "0", mf.get_remote_path()]
         )
 
 
@@ -74,8 +76,9 @@ class IMXUSBDriver(Driver, BootstrapProtocol):
     def load(self, filename=None):
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
-        filename = os.path.abspath(filename)
-        check_file(filename, command_prefix=self.loader.command_prefix)
+        mf = ManagedFile(filename, self.loader)
+        mf.sync_to_resource()
+
         subprocess.check_call(
-            self.loader.command_prefix+[self.tool, "-p", str(self.loader.path), "-c", filename]
+            self.loader.command_prefix+[self.tool, "-p", str(self.loader.path), "-c", mf.get_remote_path()]
         )

commit a995a2a05803c26cc69e25dbd78229fc5f364945

driver/quartushpsdriver: fix ManagedFile initialization
ManagedFile needs positional argument 'resource'.

Signed-off-by: Bastian Krause <bst@pengutronix.de>
Fixes: b422482a ("multiple: use ManagedFile for file uploads")

labgrid/driver/quartushpsdriver.py
@@ -63,7 +63,7 @@ class QuartusHPSDriver(Driver):
     def flash(self, filename=None, address=0x0):
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
-        mf = ManagedFile(filename)
+        mf = ManagedFile(filename, self.interface)
         mf.sync_to_resource()
 
         assert isinstance(address, int)